3250c257fc6897b2835cf96a1cd45594db8e9f45
artbataev <artbataev@gmail.com>
2018-11-12 01:18:07 +0300

    Baseline demo with deleted methods

--- src/demo.cpp
+++ src/demo.cpp
@@ -4,11 +4,19 @@
 #include <vector>
 #include <unordered_map>
 
+template <typename T>
+class DemoIteratorWithDeletedMethods: public std::vector<T>::iterator {
+public:
+//    template <typename U> bool operator==(U rhs) = delete; // EqualityComparableCheck will fail
+};
+
 int main() {
     // vector: random access iterator
     BOOST_CONCEPT_ASSERT((rai_concept::RandomAccessIteratorCheck<std::vector<int>::iterator>));
 
     // unordered_map: only forward iterator, not compiles
 //    BOOST_CONCEPT_ASSERT((rai_concept::RandomAccessIteratorCheck<std::unordered_map<int, int>::iterator>));
+
+    BOOST_CONCEPT_ASSERT((rai_concept::RandomAccessIteratorCheck<DemoIteratorWithDeletedMethods<int>>));
     return 0;
 }
